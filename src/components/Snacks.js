691a2f84af10141aa9ccac4582b612bd0fc6ffd3
Rishi Patel <rishirocks421@gmail.com>
2017-09-14 21:41:27 -0400

    changed it so list are used rather than adding a singular item everytime

--- src/components/Snacks.js
+++ src/components/Snacks.js
@@ -5,23 +5,43 @@ import popcorn from '../img/SpicyPopcornWeb.jpg';
 import peanuts from '../img/spicy_peanuts.JPG';
 import Singularsnack from './Singularsnack.js';
 
+const popcorn_ing = ["3 tbsp Sunflower Seed Oil","1 tbsp Pink Salt","1 tbsp Black Pepper", "1 tbsp chilli powder","1 tbsp tumeric","2 cups popcorn"];
+const peanuts_ing = ["2 tsp vege oil", "2 cups unsalted Peanuts","1 tbsp Pink Salt","1 tbsp Black Pepper", "1 tbsp chilli powder"];
+const popcorn_steps = ["Take a big pot and place heat on medium", "Put oil into the pot", "put popcorn seeds into pot and cover the pot", "raise heat to high","Shut off heat when seeds are done popping","Mix all spices in thoroughly"];
+const peanuts_steps = ["Heat oil on medium heat on a skillet","place in peanuts and stir for 2 mins","put in all spices and stir for 2 mins", "Let it sit on pan for 10-15 mins with no cover", "Seal in ziplock or container that does not allow air to come in"]
 
+const all_snacks = [
+  {
+    image: popcorn,
+    title: 'Popcorn',
+    ingredients: popcorn_ing,
+    instructions: popcorn_steps
+  },
+  {
+    image: peanuts,
+    title: 'Peanuts',
+    ingredients: peanuts_ing,
+    instructions: peanuts_steps
+  }
+];
+
+const Snacklist = ({all_snacks}) => (
+  <div>
+  {all_snacks.map(snack => (
+    <Singularsnack image={snack.image} title={snack.title} ingredients={snack.ingredients} instructions={snack.instructions}/>
+  ))}
+  </div>
+);
 
 class Snacks extends Component {
-
   render() {
-    var popcorn_ing = ["3 tbsp Sunflower Seed Oil","1 tbsp Pink Salt","1 tbsp Black Pepper", "1 tbsp chilli powder","1 tbsp tumeric","2 cups popcorn"];
-    var peanuts_ing = ["2 tsp vege oil", "2 cups unsalted Peanuts","1 tbsp Pink Salt","1 tbsp Black Pepper", "1 tbsp chilli powder"];
-    var popcorn_steps = ["Take a big pot and place heat on medium", "Put oil into the pot", "put popcorn seeds into pot and cover the pot", "raise heat to high","Shut off heat when seeds are done popping","Mix all spices in thoroughly"];
-    var peanuts_steps = ["Heat oil on medium heat on a skillet","place in peanuts and stir for 2 mins","put in all spices and stir for 2 mins", "Let it sit on pan for 10-15 mins with no cover", "Seal in ziplock or container that does not allow air to come in"]
     return (
       <div className="w3-black marg">
             <header className="w3-container w3-black " >
                 <h1 id="s" className="fnt w3-center w3-padding-large">Snacks</h1>
                 <hr />
             </header>
-            <Singularsnack image={popcorn} title="Popcorn" ingredients={popcorn_ing} instructions={popcorn_steps}/>
-            <Singularsnack image={peanuts} title="Peanuts" ingredients={peanuts_ing} instructions={peanuts_steps}/>
+            <Snacklist all_snacks={all_snacks} />
       </div>
     );
   }
--- src/components/non_indian.js
+++ src/components/non_indian.js
@@ -5,23 +5,44 @@ import burger from '../img/lamb_burger.jpg';
 import Basil_Rice from '../img/Thai_basil.JPG';
 import Singularsnack from './Singularsnack.js';
 
+const lamb_ing = ["1.33 lbs ground lamb","1 tbsp Pink Salt","2 tbsp Black paprika", "2 small cut onions","3 minced garlic pieces","1.5 cups of argula","3 tbsp of olive oil","1.33 cups of feta"];
+const Basil_ing = ["2 cups basil", "1 bell pepper diced","1 onion diced","4 cups jasmine rice", "8 ounces chicken","3 thai red chillies","7 garlic cloves","1 tbsp soy sauce","2 tbsp fish and oyster sauce","2 tbsp vege oil"];
+const lamb_steps = ["mix all ingredients together", "Put in the frige and let it sit for 3 hrs", "Make into Patties", "Grill"];
+const Basil_steps = ["Crush thai chilli and garlic together","whisk the 3 sauces together","place oil in wok and put on medium heat","after wok is warm place chilli garlic crush mixture","after 10 seconds or so place onions and bell peper","once onion is turned pinkish place chicken","once chicken is cooked place the whicked sauce","place basil in","put in rice","bring to high heat and mix thoroughly"]
 
+const all_non_indian = [
+  {
+    image: burger,
+    title: 'Lamb Burger',
+    ingredients: lamb_ing,
+    instructions: lamb_steps
+  },
+  {
+    image: Basil_Rice,
+    title: 'Basil Rice',
+    ingredients: Basil_ing,
+    instructions: Basil_steps
+  }
+];
+
+const NonIndianlist = ({all_non_indian}) => (
+  <div>
+  {all_non_indian.map(NonIndian => (
+    <Singularsnack image={NonIndian.image} title={NonIndian.title} ingredients={NonIndian.ingredients} instructions={NonIndian.instructions}/>
+  ))}
+  </div>
+);
 
 class NonIndian extends Component {
 
   render() {
-    var lamb_ing = ["1.33 lbs ground lamb","1 tbsp Pink Salt","2 tbsp Black paprika", "2 small cut onions","3 minced garlic pieces","1.5 cups of argula","3 tbsp of olive oil","1.33 cups of feta"];
-    var Basil_ing = ["2 cups basil", "1 bell pepper diced","1 onion diced","4 cups jasmine rice", "8 ounces chicken","3 thai red chillies","7 garlic cloves","1 tbsp soy sauce","2 tbsp fish and oyster sauce","2 tbsp vege oil"];
-    var lamb_steps = ["mix all ingredients together", "Put in the frige and let it sit for 3 hrs", "Make into Patties", "Grill"];
-    var Basil_steps = ["Crush thai chilli and garlic together","whisk the 3 sauces together","place oil in wok and put on medium heat","after wok is warm place chilli garlic crush mixture","after 10 seconds or so place onions and bell peper","once onion is turned pinkish place chicken","once chicken is cooked place the whicked sauce","place basil in","put in rice","bring to high heat and mix thoroughly"]
     return (
       <div className="w3-black marg">
             <header className="w3-container w3-black " >
                 <h1 className="fnt w3-center w3-padding-large">Other</h1>
                 <hr id="nim"/>
             </header>
-            <Singularsnack image={burger} title="Lamb Burger" ingredients={lamb_ing} instructions={lamb_steps}/>
-            <Singularsnack image={Basil_Rice} title="Basil Rice" ingredients={Basil_ing} instructions={Basil_steps}/>
+            <NonIndianlist all_non_indian={all_non_indian}/>
       </div>
     );
   }
